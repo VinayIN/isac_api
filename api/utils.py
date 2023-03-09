cc6770a1b056ac949c5735dba17c483b7783d3e2
Binay Kumar Pradhan <bpradhan.asli@gmail.com>
2023-03-09 01:53:38 +0100

    refresh token added

--- api/utils.py
+++ api/utils.py
@@ -1,29 +1,50 @@
+from typing import Optional
 import os
 from deta import Deta
-from fastapi import APIRouter, Depends, Form, HTTPException, status, Response
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm, HTTPBearer, HTTPAuthorizationCredentials
+from fastapi import APIRouter, Form, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 from passlib.context import CryptContext
-import jwt, json
-from datetime import datetime, timedelta
+import jwt
+from datetime import datetime, timedelta, timezone
 
-
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", '')
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "15"))
+REFRESH_TOKEN_VALIDITY_MINUTES = int(os.getenv("REFRESH_TOKEN_VALIDITY", "720")) # 12 hrs, after this you need to login
 
 oauth2_schema = OAuth2PasswordBearer(tokenUrl="token")
-text_db = Deta()
 router = APIRouter(
     prefix="/token",
-    tags=["utils"]
-)
+    tags=["utils"],)
+
+text_db = Deta()
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class User(BaseModel):
     email: str = "@b-tu.de"
     active_inactive: str  = "inactive"
     tag: str = "student"
+
+class CustomOAuth2PasswordRequestForm:
+    def __init__(
+        self,
+        grant_type: str = Form(default="password", regex="password|refresh_token"),
+        username: str = Form(default=None),
+        password: str = Form(default=None),
+        refresh_token: str = Form(default=None),
+        scope: str = Form(default=""),
+        client_id: Optional[str] = Form(default=None),
+        client_secret: Optional[str] = Form(default=None),
+    ):
+        self.grant_type = grant_type
+        self.username = username
+        self.password = password
+        self.refresh_token = refresh_token
+        self.scopes = scope.split()
+        self.client_id = client_id
+        self.client_secret = client_secret
 
 def authenticate_user(username, password):
     if username.endswith(("@b-tu.de", "@gmail.com")):
@@ -34,7 +55,6 @@ def authenticate_user(username, password):
                 return True
     return False
 
-
 def get_user_details(token: str=Depends(oauth2_schema)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -42,7 +62,7 @@ def get_user_details(token: str=Depends(oauth2_schema)):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        payload = jwt.decode(token, os.getenv('JWT_SECRET_KEY', ''), algorithms=[ALGORITHM])
+        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
         email = payload.get('email', "")
     except jwt.PyJWTError:
         raise credentials_exception
@@ -60,8 +80,7 @@ def admin_user(current_user: User=Depends(get_user_details)):
             detail="You dont have authorization to make changes",
             headers={"WWW-Authenticate": "Bearer"}
         )
-    return True
-    
+    return True   
 
 def generic_user(current_user: User=Depends(get_user_details)):
     if not current_user.active_inactive == "active":
@@ -72,16 +91,46 @@ def generic_user(current_user: User=Depends(get_user_details)):
         )
     return True
 
-@router.post("/", include_in_schema=False)
-async def get_token(form_data: OAuth2PasswordRequestForm=Depends()):
-    if not authenticate_user(form_data.username, form_data.password):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid credentials",
-            headers={"WWW-Authenticate": "Bearer"}
-        )
-    token_expires = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    email = form_data.username
-    access_token = jwt.encode(jsonable_encoder({"email": email, "expires_at": token_expires.isoformat()}), os.getenv('JWT_SECRET_KEY', ''), algorithm=ALGORITHM)
-    return {"access_token": access_token, "access_type": "bearer"}
-    
+@router.post("/", include_in_schema=True)
+async def get_token(form_data: CustomOAuth2PasswordRequestForm=Depends()):
+    email = ""
+    access_token = ""
+    refresh_token = form_data.refresh_token
+    if form_data.grant_type == "password":
+        email = form_data.username
+        if not authenticate_user(form_data.username, form_data.password):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid credentials",
+                headers={"WWW-Authenticate": "Bearer"}
+            )
+        access_token = jwt.encode(
+            {
+                "email": email,
+                "iat": datetime.now(tz=timezone.utc),
+                "exp": datetime.now(tz=timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)},
+            JWT_SECRET_KEY, algorithm=ALGORITHM)
+        refresh_token = jwt.encode(
+            {
+                "email": email,
+                "iat": datetime.now(tz=timezone.utc),
+                "exp": datetime.now(tz=timezone.utc) + timedelta(minutes=REFRESH_TOKEN_VALIDITY_MINUTES)},
+            JWT_SECRET_KEY, algorithm=ALGORITHM)
+    if form_data.grant_type == "refresh_token":
+        try:
+            payload = jwt.decode(form_data.refresh_token, JWT_SECRET_KEY, algorithms=[ALGORITHM])
+            email = payload.get("email")
+            access_token = jwt.encode(
+            {
+                "email": email,
+                "iat": datetime.now(tz=timezone.utc),
+                "exp": datetime.now(tz=timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES*2)},
+            JWT_SECRET_KEY, algorithm=ALGORITHM)
+        except (jwt.PyJWTError):
+            HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Please login again",
+                    headers={"WWW-Authenticate": "Bearer"}
+                )
+    refresh_token = refresh_token
+    return {"access_token": access_token, "refresh_token": refresh_token, "access_type": "bearer"}
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from typing import Union, Optional
-from fastapi import FastAPI, Form, Depends
+from fastapi import FastAPI, Depends, Request
 from fastapi.encoders import jsonable_encoder
 from api import utils
 from api.router import events, teams, news
 import warnings
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 
-app = FastAPI(title="ISAC (website API), BTU Cottbus-Senftenberg", openapi_url="/openapi.json")
+app = FastAPI(title="ISAC (website API), BTU Cottbus-Senftenberg", openapi_url="/openapi.yml")
 
 app.include_router(events.router, dependencies=[Depends(utils.generic_user)])
 app.include_router(teams.router, dependencies=[Depends(utils.generic_user)])
@@ -22,10 +22,10 @@ def root():
     return {"mesage": f"You are at homepage"}
 
 @app.post("/request")
-def request_access(data: Registration):
+async def request_access(data: Registration):
     user_info = utils.text_db.Base("user-access")
     hashed_password = utils.pwd_context.hash(data.password)
-    data.active_inactive = "inactive"
+    data.active_inactive = "inactive" # always keep this inactive, so that no one can signup unless made active in db
     encoded = jsonable_encoder(data)
 
     # do not store the password
@@ -41,5 +41,11 @@ def request_access(data: Registration):
     return encoded
 
 @app.put("/authorize", dependencies=[Depends(utils.oauth2_schema), Depends(utils.admin_user)], include_in_schema=True)
-def authorize(email):
-    return {"mesage": "Success"}
+async def authorize(email):
+    user_info = utils.text_db.Base("user-access")
+    user_data = user_info.fetch({"email": email}).items
+    if user_data:
+        for user in user_data:
+            user_info.update({"active_inactive": "active"}, key=user["key"])
+        return {"mesage": "Success"}
+    return {"message": f"The following email:{email} doesn't exists. First request authentication, then authorise."}
